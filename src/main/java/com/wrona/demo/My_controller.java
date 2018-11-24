70b5cfe50543621a3da82706a505c1daaccd6a7f
John <miloszwr2@gmail.com>
2018-11-24 14:16:51 +0100

    kilka zmian, początkowa wersja

--- src/main/java/com/wrona/demo/My_controller.java
+++ src/main/java/com/wrona/demo/My_controller.java
@@ -3,6 +3,9 @@ package com.wrona.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class My_controller {
     @GetMapping
@@ -14,5 +17,23 @@ public class My_controller {
     public String helloaggain(){
         String nowy = "czescponownie";
         return nowy;
+    }
+    @GetMapping(value = "/milosz")
+    public Person GetPerson(){
+        Person person = new Person();
+        person.setName("milosz");
+        person.setAge(19);
+        return person;
+    }
+    @GetMapping(value = "/people")
+    public List<Person> GetAllPeople(){
+        Person jacek = new Person();
+        Person wacek = new Person();
+        jacek.setName("jacek");
+        wacek.setName("wacek");
+        List <Person> MojaLista = new ArrayList<>();
+        MojaLista.add(jacek);
+        MojaLista.add(wacek);
+        return MojaLista;
     }
 }
